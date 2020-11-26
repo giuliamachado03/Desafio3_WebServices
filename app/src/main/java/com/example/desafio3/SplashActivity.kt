e28f406804da161e62b23ff7d0d05980a7ffed9a
Giulia Machado <giuliamachado03@gmail.com>
2020-11-25 22:37:28 -0300

    Splash Screen

---
+++ app/src/main/java/com/example/desafio3/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.desafio3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+const val DURACAO_SPLASH = 4000L
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }, DURACAO_SPLASH
+
+        )
+    }
+}
