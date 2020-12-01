335a19bb7a0037ed5743311fa1cd367abba265bb
Giulia Machado <giuliamachado03@gmail.com>
2020-12-01 01:43:09 -0300

    Detail Comic Fragment layout and logic

--- app/src/main/java/com/example/desafio3/comic/view/ComicFragment.kt
+++ app/src/main/java/com/example/desafio3/comic/view/ComicFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.desafio3.R
 import com.example.desafio3.comic.model.ComicDate
 import com.example.desafio3.comic.model.ComicPrice
@@ -19,7 +22,6 @@ import com.squareup.picasso.Picasso
 import java.util.*
 
 class ComicFragment : Fragment() {
-
     private lateinit var _view: View
     private lateinit var _viewModel: ComicViewModel
 
@@ -29,5 +31,78 @@ class ComicFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_comic, container, false)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _view = view
+        _viewModel = ViewModelProvider(
+            this,
+            ComicViewModel.ComicViewModelFactory(ComicRepository())
+        ).get(ComicViewModel::class.java)
+
+        val comicDescription = arguments?.getString(ComicListFragment.COMICS_DESCRIPTION)
+        val comicTitle = arguments?.getString(ComicListFragment.COMICS_TITLE)
+        val comicDates = arguments?.get(ComicListFragment.COMICS_DATES)
+        val comicPrices = arguments?.get(ComicListFragment.COMICS_PRICE)
+        val comicPages = arguments?.getInt(ComicListFragment.COMICS_PAGES)
+        val comicImages = arguments?.get(ComicListFragment.COMICS_IMAGES)
+        val comicThumbnail = arguments?.getString(ComicListFragment.COMICS_THUMBNAIL)
+        val imgLandscape = _view.findViewById<ImageView>(R.id.imgLandscape)
+        val imgComicCover = _view.findViewById<ImageView>(R.id.imgComicCover)
+        val txtTitleComic = _view.findViewById<TextView>(R.id.txtTitleComic)
+        val txtDescriptionComic = _view.findViewById<TextView>(R.id.txtDescriptionComic)
+        val txtPublished = _view.findViewById<TextView>(R.id.txtPublished)
+        val txtPrice = _view.findViewById<TextView>(R.id.txtPrice)
+        val txtPage = _view.findViewById<TextView>(R.id.txtPage)
+
+        txtTitleComic.text = comicTitle
+        if (comicDescription != null) {
+            txtDescriptionComic.text = comicDescription
+        }
+        if (comicDates != null) {
+            for (date in comicDates as List<ComicDate>) {
+                if (date.type?.contains("onsaleDate") == true){
+                    var calendar = Calendar.getInstance()
+                    calendar.time = date.date!!
+                    txtPublished.text = "${calendar.getDisplayName(
+                        Calendar.MONTH,
+                        Calendar.LONG,
+                        Locale.getDefault()
+                    )} ${calendar.get(Calendar.DAY_OF_MONTH)}, ${calendar.get(Calendar.YEAR)}"
+                }
+            }
+        }
+        if (comicPrices != null) {
+            txtPrice.text = "$ ${(comicPrices as List<ComicPrice>)[0].price.toString()}"
+        }
+        txtPage.text = comicPages?.toString()
+        Picasso.get().load(comicThumbnail).into(imgComicCover)
+        if (comicImages != null) {
+            Picasso.get().load(
+                (comicImages as List<ThumbnailModel>)[(comicImages as List<ThumbnailModel>).size - 1].getImagePath(
+                    "landscape_incredible"
+                )
+            ).into(imgLandscape)
+        }
+        setBackNavigation()
+
+        imgComicCover.setOnClickListener {
+            val navController = findNavController()
+            val bundle = bundleOf(COMIC_IMAGE to comicThumbnail)
+            navController.navigate(R.id.action_comicFragment_to_imageDetailFragment, bundle)
+        }
+    }
+
+    private fun setBackNavigation() {
+        _view.findViewById<ImageView>(R.id.imgBackComic).setOnClickListener {
+            val navController = findNavController()
+            navController.navigateUp()
+        }
+    }
+
+    companion object {
+        const val COMIC_IMAGE = "COMIC_IMAGE"
     }
 }
---
+++ app/src/main/java/com/example/desafio3/comic/view/ImageDetailFragment.kt
@@ -0,0 +1,29 @@
+package com.example.desafio3.comic.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.desafio3.R
+
+class ImageDetailFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_image_detail, container, false)
+    }
+
+    companion object {
+
+    }
+}
